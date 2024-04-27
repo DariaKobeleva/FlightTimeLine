8ebd0975d9317bb923e99362f69d0af7a932d176
KDaria <dkdva@mail.ru>
2024-04-27 15:02:17 +0200

    Initial Commit

---
+++ FlightTimeLine/FlightTimeLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimeLineApp.swift
+//  FlightTimeLine
+//
+//  Created by Дарья Кобелева on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimeLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
